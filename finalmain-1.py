da2e3ac4ac3722a594eb6448ba188b0f904dd43d
alikabdeldayem-sudo <ali.k.abdeldayem@gmail.com>
2025-12-14 21:43:09 +0200

    update

--- finalmain-1.py
+++ finalmain-1.py
@@ -12,3 +12,27 @@ def from_binary(binary_data):
         byte = binary_data[i:i + 8]
         message += chr(int(byte, 2))
     return message
+
+def encode_message(image_path, message):
+#Hide a message inside an image
+    with open(image_path, 'rb') as img:
+        image_bytes = bytearray(img.read())
+
+    binary_message = to_binary(message) + '00000000'
+
+    if len(binary_message) > (len(image_bytes) - 54):
+        print("Error: Message is too large for the image.")
+        return
+
+    index = 0
+    for i in range(54, len(image_bytes)):
+        if index < len(binary_message):
+            image_bytes[i] = (image_bytes[i] & 0xFE) | int(binary_message[index])
+            index += 1
+
+    output_path = "encoded_image.bmp"
+    with open(output_path, 'wb') as out:
+        out.write(image_bytes)
+
+    print("Message encrypted and saved as:", output_path)
+
